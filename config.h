8532b81f06a51c2725ddfa6533314e7cb2cc829a
Richard Halford <richardh.1995@googlemail.com>
2021-02-12 19:40:52 +0000

    back to clean jetbrains and siji for noto

--- config.h
+++ config.h
@@ -5,9 +5,9 @@ static int instant = 1;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=10",
+	"JetBrains Mono:size=10:antialias=true",
+  "siji:pixelsize=20"
   "Twitter Color Emoji:pixelsize=14:antialias=true:autohint=true"
-  "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
