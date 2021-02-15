1414dba1245332a25d299dd84fed121d6248382f
Richard Halford <richardh.1995@googlemail.com>
2021-02-15 12:30:05 +0000

    updated colour scheme of menu items and added fuzzy find support as default

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -11,10 +12,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#a89984", "#1d2021" },
-	[SchemeSel] = { "#1d2021", "#ebdbb2" },
-	[SchemeOut] = { "#000000", "#fb4934" },
+	/*                  fg          bg*/
+	[SchemeNorm]  =   { "#ebdbb2",  "#282828" },
+	[SchemeSel]   =   { "#1d2021",  "#458588" },
+	[SchemeOut]   =   { "#1d2021",  "#cc241d" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
